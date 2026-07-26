479fdfde640041012fbbd21e804b6f5efa3092a6
Abhay Shakya <AbhayShakya@Abhays-MacBook-Air.local>
2026-07-26 17:14:02 +0530

    Updated Send Otp Function

--- utils/sendOtp.js
+++ utils/sendOtp.js
@@ -4,6 +4,7 @@ const transporter = nodemailer.createTransport({
   host: process.env.SMTP_HOST,
   port: Number(process.env.SMTP_PORT),
   secure: false,
+  requireTLS: true,
   auth: {
     user: process.env.SMTP_USER,
     pass: process.env.SMTP_PASS,
@@ -11,35 +12,27 @@ const transporter = nodemailer.createTransport({
 });
 
 const sendOtp = async (email, otp, firstName) => {
-
   console.log("Sending OTP...");
   console.log("To:", email);
 
   try {
-
-    await transporter.verify();
-    console.log("✅ SMTP Verified");
-
     const info = await transporter.sendMail({
       from: process.env.EMAIL_FROM,
       to: email,
       subject: "Verify Your HomeSpace Account",
       html: `
-      <h2>Hello ${firstName}</h2>
-      <h1>${otp}</h1>
-      `
+        <h2>Hello ${firstName}</h2>
+        <h1>${otp}</h1>
+      `,
     });
 
-    console.log("Mail Sent:", info.messageId);
-
+    console.log("✅ Mail Sent:", info.messageId);
   } catch (err) {
-
-    console.log("SEND OTP ERROR");
+    console.log("❌ SEND OTP ERROR");
     console.log(err);
 
     throw err;
-
   }
-
 };
+
 module.exports = sendOtp;
